d9a019721f42b20ce022d22b2ac1f85bdae5cdda
Ashfaq Mahmud <ash.mse21@gmail.com>
2024-06-16 02:07:57 +0600

    update

---
+++ misc/esha.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("My name is Alvina Mahmud Oishi");
+    printf("\nI am a student of class 9");
+    printf("\nDrawing is my hobby");
+    printf("\nI like to study about Egyptian history");
+
+    return 0;
+}
