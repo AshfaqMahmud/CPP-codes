b8496141f02b0d0a5afee7543299a389fd131837
Ashfaq Mahmud <ash.mse21@gmail.com>
2024-06-30 15:42:02 +0600

    leetcode

---
+++ leetcode/asteroid_collision.cpp
@@ -0,0 +1,24 @@
+vector<int> stack;
+        
+        for (int asteroid : asteroids) {
+            bool destroyed = false;
+            
+            while (!stack.empty() && asteroid < 0 && stack.back() > 0) {
+                if (stack.back() < -asteroid) {
+                    stack.pop_back();
+                } else if (stack.back() == -asteroid) {
+                    stack.pop_back();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+            
+            if (!destroyed) {
+                stack.push_back(asteroid);
+            }
+        }
+        
+        return stack;
